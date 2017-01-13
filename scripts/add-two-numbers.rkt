d6b61916f3b3750f978b9334242f794a2f583e55
ashay-maheshwari <ashay@vlabs.ac.in>
2017-01-13 13:56:51 +0530

    add racket programs

--- scripts/add-two-numbers.rkt
+++ scripts/add-two-numbers.rkt
@@ -1,2 +1,9 @@
 #lang racket
-(+ 30 1)
+(+ 30 1) ;; add constants
+
+(define num1 30) ;; set num1 to 30
+
+(define num2 32) ;; set num2 to 32
+
+(+ num1 num2)    ;; add numbers saved in identifiers
+
---
+++ scripts/maths-expressions.rkt
@@ -0,0 +1,5 @@
+#lang racket
+(+ 2 5) ; Add 2 numbers 
+(- 20 5) ; Substract first num from second 
+(* (+ 7 6) (+ 8 8)) ; eval = (7 + 6) * (8 + 8) = 208
+(+ (* (+ 7 6) (+ 8 8)) (/ 35 5)) ; eval = ((7 + 6) * (8 + 8)) + (35 /5) = 215
