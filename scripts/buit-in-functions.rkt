d81287727607a4b5db642a5c021c91e3139a92fe
Ashay Maheshwari <Ashay Maheshwari>
2017-01-14 23:42:40 +0530

    add new racket source

---
+++ scripts/buit-in-functions.rkt
@@ -0,0 +1,16 @@
+#lang racket
+(string-append "ashay" "maheshwari") ; appends strings passed as arguments
+
+(substring "ashay maheshwari" 0 4) ; returns from index 1 to index 2 passed as arguments
+
+
+(string-length "ashay maheshwari")
+
+(string? "ashay-maheshwari") ; checks if argument passed is a string and returns #t ot #f
+
+(sqrt 16) ;; square root of 16
+
+(number? 1)  ;; check if argument is a number
+
+(equal? 6 "six") ; compares anything . Expects 2 arguments exactly
+
