93f7fa73241fb94ec02f32d1d91dd1bc02ec355b
ashay-maheshwari <ashay@vlabs.ac.in>
2017-01-16 11:40:49 +0530

    update let-exmaple

--- scripts/let-example.rkt
+++ scripts/let-example.rkt
@@ -13,3 +13,12 @@
 
 
 
+;;Example of let*
+
+(let* ([x (random 4)]
+       [o (random 4)]
+       [diff (number->string (abs (- x o)))])
+  (cond 
+    [(> x o) (string-append "X wins by " diff)]
+    [(> o x) (string-append "O wins by " diff)]
+    [else "cats; game"]))
